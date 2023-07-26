059801bd056ba2de16f72b8317b36841570e0497
Lenovo <sreejasaji64@gmail.com>
2023-07-26 10:31:45 +0530

    Initial commit

---
+++ tracker_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import Course,Coursesyllabus,Day,syllabus
+
+admin.site.register(Course)
+admin.site.register(Coursesyllabus)
+admin.site.register(Day)
+admin.site.register(syllabus)
---
+++ tracker_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+# class Course(models.Model):
+#     Course=models.CharField(max_length=200)
